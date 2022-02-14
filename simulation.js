4e46c5d9e15c2fe511c8ca28ec86a0d56a87cc7f
CDTAdmin <cdtadmin@ndmpc896.ndm.ox.ac.uk>
2022-02-14 18:37:28 +0000

    Initial files

---
+++ simulation.js
@@ -0,0 +1,77 @@
+let num = 500; // Number of points
+let range = 1; // Range of randomness
+let lambda = 0.99; // influence of memory
+let dist = 10; // closeness of infection
+
+let ax = [];
+let ay = [];
+let tendx = []; //history x coord
+let tendy = []; // histroy y coord
+let stat = []; // status of ith point
+                // 0 - uninfected, 1 - infected
+
+
+
+function setup() { // Randomly initialise the points
+  createCanvas(windowWidth, windowHeight);
+  for ( let i = 0; i < num; i++ ) { 
+    ax[i] = random(0, width);
+    ay[i] = random(0, height);
+    tendx[i] = ax[i];
+    tendy[i] = ay[i];
+    stat[i] = 0; // by default set all to unifected
+  }
+  stat[0] = 1; // make the first person infected
+  
+  frameRate(30);
+}
+
+
+function draw() {
+  background('white'); 
+  move()
+  interact()
+ 
+  // Make coordinates into points
+  for ( let i = 0; i < num; i++ ) {
+    strokeWeight(10);
+    if (stat[i]==1){
+      stroke('red');
+      point(ax[i], ay[i]);
+      continue;
+    }
+    stroke('black');
+    point(ax[i], ay[i]);
+  }
+}
+
+function move(){
+   // Randomly shift all coordinates and check  
+  for ( let i = 0; i < num; i++ ) {
+    // random component to update
+    olax = ax[i];
+    olay = ay[i];
+    ax[i] = constrain(ax[i] + random(-range, range) + lambda * (ax[i]-tendx[i]), 0, width ) ; 
+    ay[i] = constrain(ay[i] + random(-range, range) + lambda * (ay[i]-tendy[i]), 0, height) ; 
+    tendx[i] = olax;
+    tendy[i] = olay;
+  }
+}
+
+function interact(){
+  // update interactions between particles
+  for ( let i = 0; i < num; i++ ) { // go through uninfecteds
+    if (stat[i]==1){
+      continue;
+    }
+    for ( let j = 0; j < num; j++ ) { //go through infecteds
+      if (stat[j]==0 ){
+        continue;
+      }
+      if ( sqrt( (ax[i]-ax[j])**2 + (ay[i]-ay[j])**2 ) < dist ){
+        stat[i]=1;
+        break;
+      }
+    }
+  }
+}
